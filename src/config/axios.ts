b93f2abd05aa0d52b65aafa086d1470abb52a29e
Iyiolaosuagwu <iyiolaosuagwu1@gmail.com>
2023-03-31 00:17:10 +0100

    fix: removed clear error timeout

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: process.env.BASE_URL,
+    baseURL: process.env.BASE_URL || "https://cassava-backend.onrender.com",
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
